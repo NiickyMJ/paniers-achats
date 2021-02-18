73d1aa20049d1f06b73b1dcaca8fbdb8f072499c
Camille Semaan <cdem@xvitae.com>
2021-02-18 18:17:43 -0500

    Concept de base de 'routeur'. React-router-dom.

--- src/Appli.jsx
+++ src/Appli.jsx
@@ -1,41 +1,31 @@
 import './Appli.scss';
-import {useState} from 'react';
 import useLocalStorageState from './hooks/useLocalStorageState';
 import Entete from './Entete.jsx';
 import ListeProduits from './ListeProduits';
+import Accueil from './Accueil';
+import Apropos from './Apropos';
+import Contact from './Contact';
 import PiedDePage from './PiedDePage';
+import { Switch,Route } from 'react-router-dom';
 
-export default function Appli() {
-  const [compteur, setCompteur] = useState(0);
-
-  // Gérer l'état avec les "React Hooks"
-  // useState va créer une variable d'état initialisée à {}
-  // const etatPanier = useState(() => {
-  //   return JSON.parse(window.localStorage.getItem('panier')) || {};
-  //   /*
-  //   let panierDansLS = window.localStorage.getItem('panier');
-  //   if(panierDansLS !== null) {
-  //     return JSON.parse(panierDansLS);
-  //   }
-  //   else {
-  //     return {};
-  //   }*/
-  // });
-
-  // const [panier] = etatPanier;
-
-  // Sauvegarder l'état du panier dans LocalStorage
-  // Corriger avec useEffect() (hook)
-  // useEffect(() => window.localStorage.setItem('panier', JSON.stringify(panier)), [panier]);
-  
+export default function Appli() { 
   const etatPanier = useLocalStorageState({}, "panier");
-
   return (
     <div className="Appli">
       <Entete etatPanier={etatPanier} />
       <section className="contenuPrincipal">
-        <button onClick={() => setCompteur(compteur + 1)}>Compteur ({compteur})</button>
-        <ListeProduits etatPanier={etatPanier} />
+        
+        <Switch>
+          <Route path="/" component={Accueil} exact/>
+          <Route path="/nos-produits">
+            <ListeProduits etatPanier={etatPanier} exact />
+          </Route>
+          <Route path="/a-propos-de-nous" exact>
+            <Apropos />
+          </Route>
+          <Route path="/contactez-nous" component={Contact} exact />
+        </Switch>
+        
       </section>
       <PiedDePage />
     </div>
