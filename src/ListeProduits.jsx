a46f8f94ca43b077e6c846c8514eb02b49a3de58
Camille Semaan <cdem@xvitae.com>
2021-02-22 13:39:11 -0500

    Correction du problème GHPages vs routeur React.

--- src/ListeProduits.jsx
+++ src/ListeProduits.jsx
@@ -1,6 +1,19 @@
 import './ListeProduits.scss';
 import Produit from "./Produit";
 import tabProduits from './data/produits.json';
+import fbAppli from 'firebase/app';
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBAB1bpsWFNArALnECEKBRvNvkucnWE3eA",
+  authDomain: "panier-achats-react.firebaseapp.com",
+  databaseURL: "https://panier-achats-react-default-rtdb.firebaseio.com",
+  projectId: "panier-achats-react",
+  storageBucket: "panier-achats-react.appspot.com",
+  messagingSenderId: "1061179620791",
+  appId: "1:1061179620791:web:fe5c649b209e5a2a3d7c32",
+  measurementId: "G-020DCXGPX2"
+};
 
 export default function ListeProduits(props) {
   return (
--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,11 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.scss';
 import Appli from './Appli';
 
+const baseName = (window.location.href.search(/github\.io/) !== -1) ? "/panier-achats-pvt-gr2" : "";
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={baseName}>
       <Appli />
     </Router>
   </React.StrictMode>,
